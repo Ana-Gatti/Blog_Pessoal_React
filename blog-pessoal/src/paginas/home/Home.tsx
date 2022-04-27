6bb59ae6c929029708e6443167ab878278964def
Ana-Gatti <anagatti@live.com>
2022-04-27 00:44:12 -0300

    Aprendendo React

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import"./Home.css";
+
+function Home(){
+    return (
+        <>
+            <h1>Home</h1>
+            <img src="https://br.pinterest.com/pin/362258363780883338/" alt = "Imagem tela inicial"/>
+        </>
+    );
+}
+
+export default Home;
+
