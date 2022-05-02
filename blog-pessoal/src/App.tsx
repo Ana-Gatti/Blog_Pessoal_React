71d376bc13c6b3e54ea150e27de4f85aa2cf069b
Ana-Gatti <anagatti@live.com>
2022-05-01 21:15:57 -0300

    Aprendendo React

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import Home from "./paginas/home/Home"
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home'
+import Login from './paginas/login/Login'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
 
 
 function App() {
   return (
-    <Home />
-  );
+
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,14 +1,31 @@
-import React from "react";
-import"./Home.css";
+import React from 'react';
+import {Typography, Box, Grid, Button} from '@material-ui/core';
+import './Home.css';
 
-function Home(){
+function Home() {
     return (
         <>
-            <h1>Home</h1>
-            <img src="https://br.pinterest.com/pin/362258363780883338/" alt = "Imagem tela inicial"/>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant= "contained" className='botao'>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://st.depositphotos.com/1032577/4812/i/600/depositphotos_48129893-stock-photo-frame-with-daisies-and-lettering.jpg" 
+                        alt="Imagem Tela Inicial" width="500px" height="300px"/>
+                </Grid>
+                <Grid xs={12} className='postagens'>
+                </Grid>
+            </Grid>
         </>
     );
 }
 
 export default Home;
-
